ff7078a40fad31d81e0b6a80c723455bc14f947f
seyun <seyun373@gmail.com>
2022-05-10 22:37:40 +0900

    박세윤 커밋(전체적인 디자인 수정, 글 등록/수정/삭제 코드 추가)

--- HealthyWay/src/main/java/com/team1/health/vo/RecipePagingVO.java
+++ HealthyWay/src/main/java/com/team1/health/vo/RecipePagingVO.java
@@ -3,11 +3,11 @@ package com.team1.health.vo;
 public class RecipePagingVO {
 	// 페이징
 	private int onePageRecord = 8;// 한 페이지에 출력할 레코드 수
-	private int pageNum;// 현재 페이지
+	private int pageNum = 1;// 현재 페이지
 	private int totalRecord;// 총 레코드 수
 	private int totalPage;// 총 페이지 수
 	private int offsetIndex = 0;// 오프셋
-	private int onePageCount = 4; // 한 번에 표시할 페이지 수
+	private int onePageCount = 5; // 한 번에 표시할 페이지 수
 	private int startPage = 1;// 시작 페이지
 
 	// 검색
