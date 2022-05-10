cbafa7c280d62809de3047afc738ad8a0c603ef8
DABIN222 <req134679@naver.com>
2022-05-10 22:43:25 +0900

    member/login 커밋

--- HealthyWay/src/main/java/com/team1/health/dao/MemberDAO.java
+++ HealthyWay/src/main/java/com/team1/health/dao/MemberDAO.java
@@ -8,9 +8,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberDAO {
 	public MemberVO loginCheck(MemberVO vo);
-	
+
 	public int memberInsert(MemberVO vo);
-	public MemberVO checkId(MemberVO vo);
-    public MemberVO checkNick(MemberVO vo);
-    public MemberVO checkTel(MemberVO vo);
+
+	public String checkId(String user_id);
+	public String checkNick(String user_nickname);
+	public String checkTel(String tel);
+	public String checkEmail(String user_email);
+	
+	public String findId(String user_id);
+	public String findPw(String user_pw);
+	public String updatePw(String user_id);
 }
