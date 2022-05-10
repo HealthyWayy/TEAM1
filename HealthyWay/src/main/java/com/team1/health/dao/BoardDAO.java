576b91ab2a8e4deadb7ac312f2548186e8eaa09e
hansungkdy <45419374+hansungkdy@users.noreply.github.com>
2022-05-10 22:42:41 +0900

    [공지사항]

--- HealthyWay/src/main/java/com/team1/health/dao/BoardDAO.java
+++ HealthyWay/src/main/java/com/team1/health/dao/BoardDAO.java
@@ -1,6 +1,6 @@
 package com.team1.health.dao;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -9,28 +9,22 @@ import com.team1.health.vo.BoardVO;
 import com.team1.health.vo.PagingVO;
 
 
+
 @Mapper
 @Repository
-
 public interface BoardDAO {
+	
 	public int boardInsert(BoardVO vo);
-	public int totalRecord(PagingVO vo);
-	public ArrayList<BoardVO> boardSelectAll(PagingVO vo);
-	public BoardVO boardSelectByNo(int no);
-	public ArrayList<BoardVO> boardSelectNotice(int limit);
+	
+	public List<BoardVO> boardList(PagingVO pVO);
+	
+	public int totalRecord(PagingVO pVO);
+	
+	public BoardVO boardSelect(int no);
+	
+	public void hitCount(int no);
+	
 	public int boardUpdate(BoardVO vo);
-	public int boardDelete(String userid, int no);
 	
+	public int boardDelete(int no, String userid);
 }
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
