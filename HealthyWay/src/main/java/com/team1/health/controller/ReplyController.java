ef5681ea2c95b5797699e416946ffcdcb4bceaeb
Sujin Shin <97040053+JinJinee@users.noreply.github.com>
2022-05-18 16:24:46 +0900

    2022.05.18 신수진 커밋

--- HealthyWay/src/main/java/com/team1/health/controller/ReplyController.java
+++ HealthyWay/src/main/java/com/team1/health/controller/ReplyController.java
@@ -1,5 +1,41 @@
 package com.team1.health.controller;
 
-public class ReplyController {
+import java.util.List;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.team1.health.service.ReplyService;
+import com.team1.health.vo.ReplyVO;
+
+@RestController
+@RequestMapping("/reply/*")
+public class ReplyController {
+	@Inject 
+	ReplyService service;
+	
+	// 댓글목록
+	@RequestMapping("list")
+	public List<ReplyVO> list(int board_num){
+		return service.replyList(board_num);
+	}
+	
+	// 댓글등록
+	@RequestMapping(value="/reply/writeOk", method=RequestMethod.POST)
+	public int writeOk(ReplyVO vo, HttpSession session) {
+		vo.setUser_id((String)session.getAttribute("logId"));
+		
+		return service.replyWrite(vo);
+	}
+	
+	// 댓글삭제
+	@RequestMapping("del")
+	public int delOk(int reply_num, HttpSession session) {
+		String id = (String)session.getAttribute("logId");
+		return service.replyDel(reply_num, id);
+	}
 }
--- HealthyWay/src/main/webapp/js/board/pt/ptList.js
+++ HealthyWay/src/main/webapp/js/board/pt/ptList.js
@@ -1,30 +1,30 @@
 $(function(){
 	$("#recu_btn").hover(function(){
-		$(this).css("color", "#000").css("background-color", "#fff");
+		$(this).css("color", "#000").css("background-color", "#fff").css("border", "1px solid #ddd");
 	}, function(){
-		$(this).css("color", "#fff").css("background-color", "#000");
+		$(this).css("color", "#fff").css("background-color", "#A18ACB").css("border", "1px solid #A18ACB");
 	});
 	
 	$("#completed_btn").hover(function(){
-		$(this).css("color", "#fff").css("background-color", "#000");
+		$(this).css("color", "#fff").css("background-color", "#A18ACB").css("border", "1px solid #A18ACB");
 	}, function(){
-		$(this).css("color", "#000").css("background-color", "#fff");
+		$(this).css("color", "#000").css("background-color", "#fff").css("border", "1px solid #ddd");
 	});
 	
 	$("#write_btn").hover(function(){
-		$(this).css("color", "#000").css("background-color", "#fff");
+		$(this).css("color", "#000").css("background-color", "#fff").css("border", "1px solid #ddd");
 	}, function(){
-		$(this).css("color", "#fff").css("background-color", "#000");
+		$(this).css("color", "#fff").css("background-color", "#A18ACB").css("border", "1px solid #A18ACB");
 	});
 	
 	$("#recu_btn").click(function(){
-		$(".pt_post_wrap").css("display", "none");
+		$(".모집완료").css("display", "none");
 		$(".모집중").css("display", "block");
 		$(".post_head").text("모집중인 PT그룹");
 	});
 	
 	$("#completed_btn").click(function(){
-		$(".pt_post_wrap").css("display", "none");
+		$(".모집중").css("display", "none");
 		$(".모집완료").css("display", "block");
 		$(".post_head").text("모집완료 PT그룹");
 	});
@@ -33,10 +33,8 @@ $(function(){
 	
 	$(".pt_post_wrap").hover(function(){
 		$(this).css("border", "1px solid #a9a9a9");
-		$(this).children('.post_bottom').css("border-top", "1px solid #a9a9a9");
 	}, function(){
 		$(this).css("border", "1px solid #e9e9e9");
-		$(this).children('.post_bottom').css("border-top", "1px solid #e9e9e9");
 	});
 	
 	$("#searchBtn").click(function(){
